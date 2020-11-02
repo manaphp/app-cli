c663d9265809e790c49fdcb03d52d4c729fc010a
ManaPHP Mark <manaphp@qq.com>
2020-11-02 20:26:21 +0800

    Modified   app/Controllers/TestController.php Modified   config/app.php Added      .editorconfig

--- config/app.php
+++ config/app.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'id' => 'cli',
-    'env' => env('APP_ENV', 'prod'),
-    'debug' => env('APP_DEBUG', false),
-    'version' => '1.1.1',
-    'timezone' => 'PRC',
+    'id'         => 'cli',
+    'env'        => env('APP_ENV', 'prod'),
+    'debug'      => env('APP_DEBUG', false),
+    'version'    => '1.1.1',
+    'timezone'   => 'PRC',
     'master_key' => env('MASTER_KEY'),
-    'params' => [],
-    'aliases' => [
+    'params'     => [],
+    'aliases'    => [
     ],
     'components' => [
-        'db' => env('DB_URL'),
-        'redis' => env('REDIS_URL'),
+        'db'     => env('DB_URL'),
+        'redis'  => env('REDIS_URL'),
         'logger' => ['level' => env('LOGGER_LEVEL', 'info')]
     ],
 ];
